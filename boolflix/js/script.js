de9c4943b0210985d6e04a765b8c4f25e97602a4
valentina <valentinagiannone@gmail.com>
2019-11-25 15:28:15 +0100

    non va

--- boolflix/js/script.js
+++ boolflix/js/script.js
@@ -8,30 +8,36 @@ Voto
 */
 
 // eseguo chiamata api per la ricerca film
-function ricercaFilm(cerca) {
-
+function ricercaFilm(stampa) {
+ var filmAjax = stampa;
  $.ajax({
-  url:"https://api.themoviedb.org/3/search/movie?api_key=7aa608e30656e9fec4f2ce271a198fc4&language=it-ITquery=" + cerca,
+
+  url:"https://api.themoviedb.org/3/search/movie?api_key=7aa608e30656e9fec4f2ce271a198fc4&language=it-ITquery=" + filmAjax,
   method: "GET",
   success: function (film) {
+   console.log(film);
    for (var i = 0; i < film.results.length; i++) {
     console.log(film.results[i]);
    }
-  }
- });
+  },
+  error : function () {
+  alert("Errore");
+ }
 
-};
+ });
+}
+
 
 
 //inizio a portarmi avanti eseguendo la funzione legata al bottone per cercare il film
 $("#go").click(function(){
  $(".main").remove();
- var stampa = $(".search-bar").val();
- ricercaFilm(stampa);
+ var inPagina = $(".search-bar").val();
+ ricercaFilm(inPagina);
 });
 
 
 //funzione generale jquery
 $( document ).ready(function() {
- ricercaFilm(cerca);
+ ricercaFilm(filmAjax);
 });
