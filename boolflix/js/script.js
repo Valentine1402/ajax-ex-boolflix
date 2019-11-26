debeed1827ec1f7b4ff0ff35de76aa06e89d2518
valentina <valentinagiannone@gmail.com>
2019-11-26 12:55:40 +0100

    qualcosa non va

--- boolflix/js/script.js
+++ boolflix/js/script.js
@@ -1,10 +1,17 @@
 //ESERCIZIO BOOLFLIX:
+//milestone 1
 /*Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
 Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
 Titolo
 Titolo Originale
 Lingua
 Voto
+MILESTONE 2 :
+Trasformiamo il numero da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili
+
 */
 
 // eseguo chiamata api per la ricerca film
@@ -29,8 +36,8 @@ function ricercaFilm(stampa) {
      titolo: film.results[i].title,
      titoloOriginale:film.results[i].original_title,
      lingua:film.results[i].original_language,
-     voto:film.results[i].vote_average};
-     //stars: generaStelle(voto)};
+     voto:film.results[i].vote_average,
+     stars: generaStelle(voto)};
     // creo una variabile per stampare in pagina il tutto
     var stampaDati = trasferisciDati(contenitoreOggetti);
     // appendo la copia dell'oggetto per l'output in pagina
