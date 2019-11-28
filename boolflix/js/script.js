030ea2c70e01e798f4d654b78328abf98fc03939
valentina <valentinagiannone@gmail.com>
2019-11-28 12:00:48 +0100

    commenti aggiunti

--- boolflix/js/script.js
+++ boolflix/js/script.js
@@ -132,16 +132,19 @@ $( document ).ready(function() {
  ricercaSerieTv("tv");
 
  $(document).on('mouseenter', '.film', function() {
+  //al passaggio del mouse sulla miniatura si vede la copertina in anteprima
   var foto=$(this).find(".foto-copertina").attr('src');
   $('.anteprima img').attr('src',foto);
+  //al passaggio del mouse sulla miniatura si vedono le info del film e la copertina della miniatura si schiarisce (ultima parte in css)
   var miniatura=$(this).find('.ms-didascalia');
   $('.ms-didascalia').css('visibility' , 'visible');
-
+  //al passaggio del mouse sulla miniatura dovrebbero vedersi le info in anteprima
   var nome=$(this).find("ms-didascalia").html();
   $('.didascalia').html(nome);
  });
 
  $(document).on('mouseleave', '.film', function(){
+  //alla fuoriuscita del mouse spariscono le info e torna l'immagine 
   var miniatura=$(this).find('.ms-didascalia');
   $('.ms-didascalia').css('visibility' , 'hidden');
  });
